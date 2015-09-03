f994db2c0badeabdd1f6425c27b6192c9c314e12
tgsCalvin <calvinsim001@hotmail.com>
2015-09-03 12:16:19 +0800

    Event and Task class model

--- src/Event.java
+++ src/Event.java
@@ -1,5 +1,43 @@
 import java.util.*;
 
-public class Event {
 
+/**
+ * @author Sim Thiam Guan, Calvin
+ */
+
+public class Event extends Task {
+	private String description;
+	private Date start;
+	private Date end;
+	
+	public Event(int id, String task_name, String description, Date start, Date end){
+		super(id, task_name);
+		this.description = description;
+		this.start = start;
+		this.end = end;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Date getEnd() {
+		return end;
+	}
+
+	public void setEnd(Date end) {
+		this.end = end;
+	}
 }
--- src/Task.java
+++ src/Task.java
@@ -1,7 +1,28 @@
 import java.util.*;
 
-public class Task {
-	private void add(){
-		//Task data model
+/**
+ * @author Sim Thiam Guan, Calvin
+ */
+
+public abstract class Task {
+	private int id;
+	private String task_name;
+	
+	public Task(int id, String task_name){
+		this.id = id;
+		this.task_name = task_name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTask_name() {
+		return task_name;
+	}
+	public void setTask_name(String task_name) {
+		this.task_name = task_name;
 	}
 }
