89a757e22645df7cdfbe54fa9e1d7eb6586032a4
Zhou You <yollyhideout@gmail.com>
2015-09-23 23:23:57 +0800

    Implement basic parser functions without generating task_ID and exception handling

--- src/Parser.java
+++ src/Parser.java
@@ -1,97 +1,178 @@
 import java.util.*;
-import CommandType;
-
 /**
  * @author Zhou You
  */
+ 
+ /**
+ * Parser API demo:
+ * Construct:
+ * Parser parseResult = new Parser(inputString);
+ *
+ * Method:
+ * getCommandType(): return commandType in String
+ * 
+ * for add cmd:
+ * getTaskType():floatingTask, eventTask, deadlineTask
+ * getTaskID()
+ * getTaskName()
+ * getDescription()
+ * getDate()
+ * getStartTime()
+ * getEndTime()
+ *
+ * for delete cmd:
+ * getIndex()
+ * 
+ * for display cmd:
+ */
 
 public class Parser {
-	/* Attribute */
-	private userInput;
+	/* =========== Attribute ===========*/
+	private String userInput;
 	private String[] inputArr;
+	private String task_id;
+	private String task_name;
+	private String description;
+	private String date;
+	private String start_time;
+	private String end_time;
 	
 	
-	/* Constructor */
+	/* =========== Constructor ===========*/
 	public Parser(String userInput){
 		this.userInput = userInput;
 		this.inputArr = userInput.split(" ");
+		parse(userInput);
 	}
 	
-	/* Parser API */
+	/* =========== Parser API ===========*/
+	protected String getCommandType(){
+		return getFirstWord(userInput);
+	}
+	
+	/* special method for add cmd */
+
+	//general
+	protected String getTaskType(){
+		String type = inputArr[1];
+		switch (type) {
+			case "-f":
+				return "floatingTask";
+			case "-e":
+				return "eventTask";
+			case "-d":
+				return "deadlineTask";
+			default:
+				throw new Error("Unrecognized task type");
+		}
+	}
+	
 	protected String getTaskID(){
-		//use a randomised generator to assign an ID to each task		
+		//use a randomised generator to assign an ID to each task
+		return task_id;		
 	} 
 	
 	protected String getTaskName(){
-		
+		return task_name;
 	}
 	
 	protected String getDescription(){
-		
+		return description;
 	}
 	
+	//event task
 	protected String getDate(){
-		
+		return date;
 	}
 	
+	//event task
 	protected String getStartTime(){
-		
+		return start_time;
 	}
 	
+	//event task
 	protected String getEndTime(){
-		
+		return end_time;	
+	}
+	
+	//deadline task
+	protected String getDeadlineTime(){
+		return end_time;
+	}
+	
+	
+	
+	/* special API for delete cmd */
+	protected String getIndex(){
+		return task_id;	
 	}
 	
 	
 	/* Helper method */
-	private String getFirstWord(){
-		CommandType commandType = determineCommandType(commandTypeString);
-		
-		switch (commandType) {
-			case ADD:
-				return add(userCommand);
-			case DISPLAY:
-				return display(userCommand);
-			case CLEAR:
-				return clear(userCommand);
-			case DELETE:
-				return delete(userCommand);
-			case INVALID:
-				return String.format(MESSAGE_INVALID_FORMAT, userCommand);
-			case EXIT:
-				System.exit(0);
-			case SORT:
-				return sort(userCommand);
-			case SEARCH:
-				return search(userCommand);
+	private void parse(String userInput){
+		String cmdType = getCommandType();
+		switch (cmdType) {
+			case "add":
+				parseAddCommand(userInput);
+				break;
+			case "delete":
+				parseDeleteCommand(userInput);
+				break;
+			case "display":
+				parseDisplayCommand(userInput);
+				break;
 			default:
 			  throw new Error("Unrecognized command type");
 		}
 	}
 	
-	private static CommandType determineCommandType(String commandTypeString) {
-		if(commandTypeString == null){
-			throw new Error("Command type string cannot be null!");
-		}
+	private void parseAddCommand(String userInput){
+		this.task_id = getTaskID();
+		this.task_name = inputArr[2];
+		this.description = inputArr[3];
 		
-		if(commandTypeString.equalsIgnoreCase("add")) {
-			return CommandType.ADD;
-		} else if (commandTypeString.equalsIgnoreCase("DISPLAY")) {
-			return CommandType.DISPLAY;
-		} else if (commandTypeString.equalsIgnoreCase("CLEAR")) {
-			return CommandType.CLEAR;
-		} else if (commandTypeString.equalsIgnoreCase("EXIT")) {
-			return CommandType.EXIT;
-		} else if(commandTypeString.equalsIgnoreCase("DELETE")) {
-			return CommandType.DELETE;
-		} else if(commandTypeString.equalsIgnoreCase("SORT")) { 
-			return CommandType.SORT;
-		} else if(commandTypeString.equalsIgnoreCase("SEARCH")) {
-			return CommandType.SEARCH;	
-		} else {
-			return CommandType.INVALID;
+		String type = inputArr[1];
+		if(type.equals("-f")) {
+			parseFloatingTask();
+		}
+		if(type.equals("-e")) {
+			parseEventTask();
+		} 
+		if(type.equals("-d")){
+			parseDeadlineTask();
 		}
 	}
+	
+	private void parseDeleteCommand(String userInput){
+		this.task_id = inputArr[1];
+	}
+	
+	private void parseDisplayCommand(String userInput){
+		
+	}
+	
+	private void parseFloatingTask(){
+
+	}
+	
+	private void parseEventTask(){
+		this.date = inputArr[4];
+		this.start_time = inputArr[5];
+		this.end_time = inputArr[6];
+	}
+	
+	private void parseDeadlineTask(){
+		this.date = inputArr[4];
+		this.end_time = inputArr[5];
+	}
+	
+	private String getFirstWord(String commandTypeString){
+		String[] strArr = commandTypeString.split(" ");
+		String commandType = strArr[0];
+		return commandType;
+	
+	}
+
 	
 	
 }
