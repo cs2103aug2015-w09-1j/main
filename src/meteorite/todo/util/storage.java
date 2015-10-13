46fb70d2e412920691607781734af78d7ef2deba
Pure_Sero <pure943597795@gmail.com>
2015-10-13 23:12:44 +0800

    update storage

--- src/meteorite/todo/util/storage.java
+++ src/meteorite/todo/util/storage.java
@@ -8,7 +8,6 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.util.ArrayList;
 
 public class storage {
 
