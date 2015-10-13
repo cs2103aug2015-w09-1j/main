4a499fe9f7bfa3e57b2001f1da11aa77a7e6e964
Pure_Sero <pure943597795@gmail.com>
2015-10-13 23:11:22 +0800

    update storage

--- src/meteorite/todo/util/storage.java
+++ src/meteorite/todo/util/storage.java
@@ -8,51 +8,64 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class storage {
 
-	/*
-	 * @SuppressWarnings({ "unchecked", "resource" }) public static
-	 * ArrayList<Task> loadList(String path) { try { ObjectInputStream
-	 * fileToLoad = new ObjectInputStream(new FileInputStream(path));
-	 * ArrayList<Task> loadedList = (ArrayList<Task>) fileToLoad.readObject();
-	 * return loadedList; } catch (Exception e) { e.printStackTrace(); } return
-	 * null; }
-	 * 
-	 * 
-	 * @SuppressWarnings("resource") public static void saveList(ArrayList<Task>
-	 * listToSave, String path) { try { ObjectOutputStream fileToSave = new
-	 * ObjectOutputStream(new FileOutputStream(path));
-	 * fileToSave.writeObject(listToSave); fileToSave.flush();
-	 * 
-	 * } catch (Exception e) { e.printStackTrace(); }
-	 * 
-	 * }
-	 */
+	private static String path="";
+	private static String fileName=null;
 
-	// For test
-
-	public static void write(Object f, String filename) throws Exception {
-		XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(filename)));
-		encoder.writeObject(f);
-		encoder.close();
+	public static void setPath(String path) {
+		storage.path = path;
+		return;
 	}
 
-	public static Object read(String filename) throws Exception {
-		XMLDecoder decoder = new XMLDecoder(new BufferedInputStream(new FileInputStream(filename)));
-		Object o = decoder.readObject();
-		decoder.close();
-		return o;
+	public static void setfileName(String fileName) {
+		storage.fileName = fileName;
+		return;
+	}
+
+	public static String getPath() {
+		return path;
+	}
+
+	public static String getfileName() {
+		return fileName;
+	}
+
+	public static void save(Object f) {
+		try {
+			XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(path+fileName)));
+			encoder.writeObject(f);
+			encoder.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return;
+	}
+
+	public static Object load() throws Exception {
+		try {
+			XMLDecoder decoder = new XMLDecoder(new BufferedInputStream(new FileInputStream(path+fileName)));
+			Object o = decoder.readObject();
+			decoder.close();
+			return o;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+
 	}
 
 	public static void main(String args[]) throws Exception {
-		ArrayList<Task> listToSave = new ArrayList<Task>();
 		Task a = new Task("xxxx", "yyyyy");
 
-		storage.write(a, "foo.fxml");
+		storage.setPath("..\\");
+		storage.setfileName("test.fxml");
+		storage.save(a);
+		storage.setPath("doncare\\");
+		storage.setfileName("test.fxml");
+		storage.save(a);
 		return;
 
 	}
