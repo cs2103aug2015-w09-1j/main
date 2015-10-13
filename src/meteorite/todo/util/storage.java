435478d0a81ae5f68c98a4262188e4ee2bcc4d29
Pure_Sero <pure943597795@gmail.com>
2015-10-13 23:16:54 +0800

    update storage

--- src/meteorite/todo/util/storage.java
+++ src/meteorite/todo/util/storage.java
@@ -11,7 +11,6 @@ import java.io.FileOutputStream;
 
 public class storage {
 
-<<<<<<< HEAD
 	private static String path="";
 	private static String fileName=null;
 
@@ -54,56 +53,18 @@ public class storage {
 			e.printStackTrace();
 		}
 		return null;
-
-=======
-	/*
-	 * @SuppressWarnings({ "unchecked", "resource" }) public static
-	 * ArrayList<Task> loadList(String path) { try { ObjectInputStream
-	 * fileToLoad = new ObjectInputStream(new FileInputStream(path));
-	 * ArrayList<Task> loadedList = (ArrayList<Task>) fileToLoad.readObject();
-	 * return loadedList; } catch (Exception e) { e.printStackTrace(); } return
-	 * null; }
-	 * 
-	 * 
-	 * @SuppressWarnings("resource") public static void saveList(ArrayList<Task>
-	 * listToSave, String path) { try { ObjectOutputStream fileToSave = new
-	 * ObjectOutputStream(new FileOutputStream(path));
-	 * fileToSave.writeObject(listToSave); fileToSave.flush();
-	 * 
-	 * } catch (Exception e) { e.printStackTrace(); }
-	 * 
-	 * }
-	 */
-
-	// For test
-
-	public static void save(Object f, String filename) throws Exception {
-		XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream(filename)));
-		encoder.writeObject(f);
-		encoder.close();
 	}
-
-	public static Object load(String filename) throws Exception {
-		XMLDecoder decoder = new XMLDecoder(new BufferedInputStream(new FileInputStream(filename)));
-		Object o = decoder.readObject();
-		decoder.close();
-		return o;
->>>>>>> origin/storage
-	}
-
+	
 	public static void main(String args[]) throws Exception {
 		Task a = new Task("xxxx", "yyyyy");
 
-<<<<<<< HEAD
 		storage.setPath("..\\");
 		storage.setfileName("test.fxml");
 		storage.save(a);
 		storage.setPath("doncare\\");
 		storage.setfileName("test.fxml");
 		storage.save(a);
-=======
-		storage.save(a, "foo.fxml");
->>>>>>> origin/storage
+
 		return;
 
 	}
