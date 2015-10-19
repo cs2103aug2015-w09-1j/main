2e502ab8bdfa8ebade9d4c24249c297acd423d71
Jason <cvansang94@gmail.com>
2015-10-19 10:27:43 +0800

    Add Parser

--- src/util/CommandParser.java
+++ src/util/CommandParser.java
@@ -1,139 +1,380 @@
+//the follow is the list of command:
+//	1) add
+//		a.with deadline
+//			add <task name> by <date> <time>
+//		b.event
+//			add <task name> from <date><time> to <date><time>
+//		c.floating
+//			add <task name>
+//	
+//	2) delete
+//		delete <id>
+//	
+//	3) search
+//		a. search tasks on a certain date
+//			search on <date>
+//		b. search by task name
+//			search <keyword>
+//	
+//	4) display / showall
+//		a. display archived tasks
+//			display archived
+//		b. display all incomplete tasks
+//			display all
+//	
+//	5) edit
+//		a. reset a task. all attribute will be set as null
+//			edit <id>
+//	    b. edit the whole task
+//	    	edit <id> <all information>
+//	
+//	6) undo
+//		undo
+//		
+//	7) complete
+//		complete <id> -- mark the task as complete
+//		
+//	8) archive
+//		archive <id>
+//	
+//	9) set storage path
+//		set <storage path>
+//	
+//	10) help
+//		help
+//	
+//	11) load
+//		load
+//		
+//	12) save
+//		save
+
+
 package meteorite.todo.util;
 
-
-
 public class CommandParser {
-	//Attributes
+	//Attribute
 	private String userInput;
 	private String command;
-	private String taskType;
 	private String args;
-	private String[] argArray;
-	private String[] inputArray;
+	
 	private String taskName;
 	private String startTime;
 	private String endTime;
 	private String startDate;
 	private String endDate;
-	private int id;
+	private int taskID;
+	private String searchWord;
+	private String searchDate;
+	private String displayMode;
+	private String storagePath;
+	
+	CommandChecker cc;
 	
 	//Constructor
 	public CommandParser(String userInput) {
 		this.setUserInput(userInput);
-		this.inputArray = userInput.split(" ", 3);
-		this.command = inputArray[0];
-		this.setTaskType(inputArray[1]);
-		this.args = inputArray[2];
-		this.argArray = args.split("from |to |by ");
-		parse(userInput);
+		cc = new CommandChecker(userInput);
+		if (!cc.isValid()) {
+			throw new Error(cc.getErrorMessage());
+		} else {
+			 this.setCommandType();
+			 this.setArgs();
+			 parse();
+		} 			
 	}
-	
 	public CommandParser() {
-		// TODO Auto-generated constructor stub
+		
 	}
-	
-	
-	
-	public String getUserInput() {
-		return userInput;
-	}
-
-	public void setUserInput(String userInput) {
-		this.userInput = userInput;
-	}
-
+	//public methods
 	public String getCommandType() {
-		return command;
+		return this.command;
 	}
-	public int getId() {
-		return id;
-	}
-	
 	public String getTaskName(){
-		return taskName;
+		return this.taskName;
 	}
-	
-	public String getTaskType() {
-		return taskType;
+	public int getId(){
+		return this.taskID;
 	}
-
-	public void setTaskType(String taskType) {
-		this.taskType = taskType;
+	public String getErrorMessage(){
+		return cc.getErrorMessage();
 	}
-
-	//event task
 	public String getStartDate(){
-		return startDate;
+		return this.startDate;
 	}
-	//event task
-	public String getEndDate() {
-		return endDate;
+	public String getEndDate(){
+		return this.endDate;
 	}
-	
-	
-	//event task
 	public String getStartTime(){
-		return startTime;
+		return this.startTime;
 	}
-	
-	//event task
 	public String getEndTime(){
-		return endTime;	
+		return this.endTime;
+	}
+	public String getSearchDate(){
+		return this.searchDate;
+	}
+	public String getSearchWord(){
+		return this.searchWord;
+	}
+	public String getDisplayMode(){
+		return this.displayMode;
+	}
+	public String getStoragePath(){
+		return this.storagePath;
 	}
 	
 	
-	/* Helper method */
-	private void parse(String userInput){
+	
+	//private methods
+	private void parse(){
 		String cmdType = getCommandType();
 		switch (cmdType) {
 			case "add":
-				parseAddCommand(userInput);
+				parseAddCommand(getArgs());
 				break;
 			case "delete":
-				parseDeleteCommand(userInput);
+				parseDeleteCommand();
 				break;
 			case "display":
-				parseDisplayCommand(userInput);
+				parseDisplayCommand();
+				break;
+			case "search":
+				parseSearchCommand();
+				break;
+			case "undo":
+				parseUndoCommand();
+				break;
+			case "help":
+				parseHelpCommand();
+				break;
+			case "complete":
+				parseCompleteCommand();
+				break;
+			case "archive":
+				parserArchiveCommand();
+				break;
+			case "set":
+				parseSetCommand();
+				break;
+			case "edit":
+				parseEditCommand();
+				break;
+			case "save":
+				parseSaveCommand();
+				break;
+			case "load":
+				parseLoadCommand();
 				break;
 			default:
-			  throw new Error("Unrecognized command type");
+				throw new Error("command not recognised: "+cmdType);
 		}
 	}
 	
-	private void helper(int pos, String str) {
-		switch(pos) {
-		case 0:this.taskName = str;
-		       break;
-		case 1:this.startTime = str;
-		       break;
-		case 2:this.endTime = str;
-			   break;
-		case 3:this.startDate = str;
-		       break;
-		case 4:this.endDate = str;
-		       break;
-		}
-	}
-	
-	private void parseAddCommand(String userInput) {
-		int len = this.argArray.length;
-		for (int i = 0;i < len;i++) {
-			helper(i, this.argArray[i]);
-		}
-	}
-
-	private void parseDeleteCommand(String userInput) {
-		this.id = Integer.parseInt(this.argArray[0]);
-	}
-	
-	private void parseDisplayCommand(String userInput){
+	private void parseSaveCommand(){
 		
 	}
 	
-	//testing purpose
-	public static void main(String[] args) {
-		CommandParser cp = new CommandParser("add meeting with boss from 9 to 10 by Monday");
-		System.out.println(cp.getStartTime());
+	private void parseLoadCommand(){
+		
 	}
+	
+	private void parseEditCommand(){
+		String args = getArgs();
+		String[] argArray = args.split(" ", 2);
+		if(argArray.length == 1) {
+			setTaskID(Integer.parseInt(args));
+		} else {
+			String id = argArray[0];
+			setTaskID(Integer.parseInt(id));
+			parseAddCommand(argArray[1]);
+		}
+	}
+	
+	private void parseSetCommand(){
+		String args = getArgs();
+		setStoragePath(args);
+	}
+	
+	private void parserArchiveCommand() {
+		String args = getArgs();
+		setTaskID(Integer.parseInt(args));
+		
+	}
+	private void parseCompleteCommand() {
+		String args = getArgs();
+		setTaskID(Integer.parseInt(args));
+		
+	}
+	private void parseHelpCommand(){
+		
+	}
+	
+	private void parseUndoCommand(){
+		
+	}
+	
+	private void parseSearchCommand() {
+		String args = getArgs();
+		if(args.contains("on")) {
+			String date = args.substring(3);
+			setSearchDate(date);
+		} else {
+			setSearchWord(args);
+		}
+		
+	}
+	private void parseAddCommand(String args){
+		String taskType = getTaskType(args);
+		switch (taskType){
+			case "float":
+				parseFloatTask(args);
+				break;
+			case "deadline":
+				parseDeadlineTask(args);
+				break;
+			case "event":
+				parseEventTask(args);
+				break;
+			default:
+				throw new Error("task type not recognised");		
+		}
 
+	}
+	
+	private void parseDeleteCommand(){
+		String args = getArgs();
+		setTaskID(Integer.parseInt(args));
+	}
+	
+	private void parseDisplayCommand(){
+		String args = getArgs();
+		if(args.equals("archived")) {
+			setDisplayMode(args);
+		} else if(args.equals("all")) {
+			setDisplayMode(args);
+		} else {
+			throw new Error("display cmd invalid");
+		}
+	}
+	
+	private void setDisplayMode(String args) {
+		this.displayMode = args;
+	}
+	private static String getTaskType(String input){
+		if(input.contains("from")) {
+			return "event";
+		} else if (input.contains("by")) {
+			return "deadline";
+		} else {
+			return "float";
+		}	
+	}
+	
+	private void parseFloatTask(String args){
+		setTaskName(args);
+	}
+	
+	private void parseEventTask(String args) {
+		//add meeting from 2015-10-03 0900 to 2015-10-04 0900
+		String[] argsArray = args.split("from |to");
+		String[] startArr = argsArray[1].split(" ");
+		String[] endArr = argsArray[2].trim().split(" ");
+		
+		setTaskName(argsArray[0]);
+		setStartDate(startArr[0]);
+		setStartTime(startArr[1]);
+		setEndDate(endArr[0]);
+		setEndTime(endArr[1]);
+
+	}
+	
+	private void parseDeadlineTask(String args) {
+		//add finish project manual by 2015-10-03 0900
+		String[] argsArray = args.split(" by ");
+		String[] endArr = argsArray[1].split(" ");
+		setTaskName(argsArray[0]);
+		setEndDate(endArr[0]);
+		setEndTime(endArr[1]);
+	}
+	
+	private void setUserInput(String userInput){
+		this.userInput = userInput;
+	}
+	private void setCommandType(){
+		String[] inputArr = this.userInput.split(" ", 2);
+		this.command = inputArr[0].trim();  
+	}
+	private void setArgs(){
+		if(!this.command.equals("undo") && !this.command.equals("help")
+				&& !this.command.equals("load") && !this.command.equals("save")){
+			String[] inputArr = this.userInput.split(" ", 2);
+			this.args = inputArr[1];
+		}
+	}
+	private String getArgs() {
+		return this.args;
+	}
+	private void setTaskName(String name) {
+		this.taskName = name.trim();
+	}
+	private void setStartTime(String time) {
+		this.startTime = time.trim();
+	}
+	private void setEndTime(String time) {
+		this.endTime = time.trim();
+	}
+	private void setStartDate(String date) {
+		this.startDate = date.trim();
+	}
+	private void setEndDate(String date) {
+		this.endDate = date.trim();
+	}
+	private void setTaskID(int id) {
+		this.taskID = id;
+	}
+	private void setSearchDate(String date) {
+		this.searchDate = date;
+	}	
+	private void setSearchWord(String word) {
+		this.searchWord = word;
+	}
+	private void setStoragePath(String path){
+		this.storagePath = path;
+	}
+	
+	static void print(String[] str){
+		for(int i=0;i<str.length;i++){
+			System.out.println("Index "+i+" : "+ str[i]);
+		}
+	}
+	public static void main(String[] args) {
+//		CommandParser cp2 = new CommandParser("display archived");
+		String str = "2 meeting with boss by 2015-02-03 1259";
+		String[] strArr = str.split(" ", 2);
+		print(strArr);
+//		System.out.println(cp2.getCommandType());
+		
+	}
+	
+
+}
+
+class CommandChecker {
+	//Attribute
+	private boolean isValid;
+	private String errorMessage;
+	
+	//Constructor
+	public CommandChecker(String userInput) {
+		this.isValid = true;
+	}
+	
+	public boolean isValid(){
+		return this.isValid;
+	}
+	
+	public String getErrorMessage(){
+		return this.errorMessage;
+	}
 }
---
+++ src/util/CommandParserTest.java
@@ -0,0 +1,223 @@
+package meteorite.todo.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommandParserTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testCommandParserString() {
+	}
+
+	@Test
+	public void testCommandParser() {
+	}
+
+	@Test
+	public void testGetCommandType() {
+		//event task
+		CommandParser cp1 = new CommandParser("add meeting from 2015-10-03 0900 to 2015-10-04 0900");
+		assertEquals("add", cp1.getCommandType());
+		
+		//deadline task
+		CommandParser cp2 = new CommandParser("add finish project manual by 2015-10-03 0900");
+		assertEquals("add", cp2.getCommandType());
+		
+		//float task
+		CommandParser cp3 = new CommandParser("add visit uncle Lee");
+		assertEquals("add", cp3.getCommandType());
+		
+		//delete
+		CommandParser cp4 = new CommandParser("delete 2");	
+		assertEquals("delete", cp4.getCommandType());
+		
+		
+		//display
+		CommandParser cp5 = new CommandParser("display archived");	
+		assertEquals("display", cp5.getCommandType());
+		
+		//search
+		CommandParser cp6 = new CommandParser("search meeting");
+		assertEquals("search", cp6.getCommandType());
+		
+		CommandParser cp7 = new CommandParser("search on 2015-10-03");
+		assertEquals("search", cp7.getCommandType());
+		
+		//undo
+		CommandParser cp8 = new CommandParser("undo");
+		assertEquals("undo", cp8.getCommandType());
+		
+		//help
+		CommandParser cp9 = new CommandParser("help");
+		assertEquals("help", cp9.getCommandType());
+		
+		//complete
+		CommandParser cp10 = new CommandParser("complete 10");
+		assertEquals("complete", cp10.getCommandType());
+		
+		//archive 
+		CommandParser cp11 = new CommandParser("archive 11");
+		assertEquals("archive", cp11.getCommandType());
+		
+		//set
+		CommandParser cp12 = new CommandParser("set C:/Program/SilentJarvis/Data");
+		assertEquals("set", cp12.getCommandType());
+		
+		//edit
+		CommandParser cp13 = new CommandParser("edit 2");
+		assertEquals("edit", cp13.getCommandType());
+		
+		CommandParser cp14 = new CommandParser("edit 2 meeting with boss by 2015-03-04 1259");
+		assertEquals("edit", cp14.getCommandType());
+		
+		//save
+		CommandParser cp15 = new CommandParser("save");
+		assertEquals("save", cp15.getCommandType());
+		
+		//load
+		CommandParser cp16 = new CommandParser("load");
+		assertEquals("load", cp16.getCommandType());
+		
+	}
+	
+	@Test
+	public void testGetStoragePath(){
+		CommandParser cp1 = new CommandParser("set C:/Program/SilentJarvis/Data");
+		assertEquals("C:/Program/SilentJarvis/Data", cp1.getStoragePath());
+	}
+
+	@Test
+	public void testGetTaskName() {
+		//event task
+		CommandParser cp1 = new CommandParser("add meeting from 2015-10-03 0900 to 2015-10-04 0900");
+		assertEquals("meeting", cp1.getTaskName());
+		
+		//deadline task
+		CommandParser cp2 = new CommandParser("add finish project manual by 2015-10-03 0900");
+		assertEquals("finish project manual", cp2.getTaskName());
+		
+		//float task
+		CommandParser cp3 = new CommandParser("add visit uncle Lee");
+		assertEquals("visit uncle Lee", cp3.getTaskName());
+		
+		//edit
+		CommandParser cp4 = new CommandParser("edit 2 meeting with boss by 2015-03-04 1259");
+		assertEquals("meeting with boss", cp4.getTaskName());
+
+	}
+	
+	@Test
+	public void testGetDisplayMode(){
+		//archive
+		CommandParser cp1 = new CommandParser("display archived");
+		assertEquals("archived", cp1.getDisplayMode());
+		
+		//all
+		CommandParser cp2 = new CommandParser("display all");
+		assertEquals("all", cp2.getDisplayMode());
+	}
+	
+	
+	@Test
+	public void testGetSearchWord() {
+		CommandParser cp1 = new CommandParser("search meeting");
+		assertEquals("meeting", cp1.getSearchWord());
+
+	}
+	
+	@Test
+	public void testGetSearchDate() {
+		CommandParser cp1 = new CommandParser("search on 2015-10-03");
+		assertEquals("2015-10-03", cp1.getSearchDate());
+
+	}
+	
+	
+	@Test
+	public void testGetId() {
+		int id = 2;
+		//delete
+		CommandParser cp1 = new CommandParser("delete 2");
+		assertEquals(id, cp1.getId());
+		
+		//complete
+		CommandParser cp2 = new CommandParser("complete 2");
+		assertEquals(id, cp2.getId());
+		
+		//archive
+		CommandParser cp3 = new CommandParser("archive 2");
+		assertEquals(id, cp3.getId());
+		
+		//edit
+		CommandParser cp4 = new CommandParser("edit 2");
+		assertEquals(id, cp4.getId());
+
+	}
+
+	@Test
+	public void testGetErrorMessage() {
+
+	}
+
+	@Test
+	public void testGetStartDate() {
+		//event task
+		CommandParser cp1 = new CommandParser("add meeting from 2015-10-03 0900 to 2015-10-04 0900");
+		assertEquals("2015-10-03", cp1.getStartDate());
+		
+//		
+//		CommandParser cp2 = new CommandParser("add meeting with boss from  to 10 on Monday");
+//		assertEquals("Monday", cp2.getStartDate());
+		
+		//deadline task
+		CommandParser cp3 = new CommandParser("add finish project manual by 2015-10-03 0900");
+		assertEquals(null, cp3.getStartDate());
+	}
+
+	@Test
+	public void testGetEndDate() {
+		//event task
+		CommandParser cp1 = new CommandParser("add meeting from 2015-10-03 0900 to 2015-10-04 0900");
+		assertEquals("2015-10-04", cp1.getEndDate());
+		
+		//deadline task
+		CommandParser cp2 = new CommandParser("add finish project manual by 2015-10-03 0900");
+		assertEquals("2015-10-03", cp2.getEndDate());
+		
+		//edit
+		CommandParser cp3 = new CommandParser("edit 2 meeting with boss by 2015-03-04 1259");
+		assertEquals("2015-03-04", cp3.getEndDate());
+	}
+
+	@Test
+	public void testGetStartTime() {
+		//event task
+		CommandParser cp = new CommandParser("add meeting from 2015-10-03 0900 to 2015-10-04 0900");
+		assertEquals("0900", cp.getStartTime());
+		
+		//deadline task
+		CommandParser cp2 = new CommandParser("add finish project manual by 2015-10-03 0900");
+		assertEquals(null, cp2.getStartDate());
+	}
+
+	@Test
+	public void testGetEndTime() {
+		CommandParser cp = new CommandParser("add meeting from 2015-10-03 0900 to 2015-10-04 0900");
+		assertEquals("0900", cp.getEndTime());
+		
+		//deadline task
+		CommandParser cp2 = new CommandParser("add finish project manual by 2015-10-03 0900");
+		assertEquals("0900", cp2.getEndTime());
+		
+		//edit
+		CommandParser cp3 = new CommandParser("edit 2 meeting with boss by 2015-03-04 1259");
+		assertEquals("1259", cp3.getEndTime());
+	}
+
+}
