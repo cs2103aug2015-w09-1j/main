5b5f906df91df132bb00f0bc08376bda1de450e5
Pure_Sero <pure943597795@gmail.com>
2015-11-03 20:44:40 +0800

    update testing

--- src/test/SystematicTest.java
+++ src/test/SystematicTest.java
@@ -39,11 +39,59 @@ public class SystematicTest {
 		
 		Controller.executeCMD("add A");
 		Controller.executeCMD("add D from 2014-11-20 1800 to 2014-12-01 0700");
-		Controller.executeCMD("add C by 1500");
+		Controller.executeCMD("add C by 2015-11-01 1500");
 		Controller.executeCMD("add B from 2015-12-10 0300 to 2015-12-12 0500");
-		Controller.executeCMD("add ABC");
+		Controller.executeCMD("add ABC");		
+		taskList=Controller.getTaskList();
+		
+		assertTrue((taskList.get(0))instanceof FloatingTask);
+		assertTrue(taskList.get(0).getTaskName().equals("A"));
+		assertTrue((taskList.get(1))instanceof FloatingTask);
+		assertTrue(taskList.get(1).getTaskName().equals("ABC"));
+		
+		EventTask temp1=(EventTask) taskList.get(2);
+		assertTrue((taskList.get(2))instanceof EventTask);
+		assertTrue(temp1.getTaskName().equals("B"));
+		assertTrue(temp1.getStartDate().equals("2015-12-10"));
+		assertTrue(temp1.getStartTime().equals("03:00"));
+		assertTrue(temp1.getEndDate().equals("2015-12-12"));
+		assertTrue(temp1.getEndTime().equals("05:00"));
+		
+		
+		DeadlineTask temp2=(DeadlineTask) taskList.get(3);
+		assertTrue((taskList.get(3))instanceof DeadlineTask);
+		assertTrue(temp2.getTaskName().equals("C"));
+		assertTrue(temp2.getDeadlineDate().equals("2015-11-01"));
+		assertTrue(temp2.getDeadlineTime().equals("15:00"));
+		
+		EventTask temp3=(EventTask) taskList.get(4);
+		assertTrue((taskList.get(4))instanceof EventTask);
+		assertTrue(temp3.getTaskName().equals("D"));
+		assertTrue(temp3.getStartDate().equals("2014-11-20"));
+		assertTrue(temp3.getStartTime().equals("18:00"));
+		assertTrue(temp3.getEndDate().equals("2014-12-01"));
+		assertTrue(temp3.getEndTime().equals("07:00"));
+		
 		Controller.executeCMD("delete 1");
+		taskList=Controller.getTaskList();
+		
+		assertTrue((taskList.get(0))instanceof FloatingTask);
+		assertTrue(taskList.get(0).getTaskName().equals("ABC"));
+		
+		assertTrue((taskList.get(1))instanceof EventTask);
+		assertTrue(temp1.getTaskName().equals("B"));
+		
+		assertTrue((taskList.get(2))instanceof DeadlineTask);
+		assertTrue(temp2.getTaskName().equals("C"));
+
+		assertTrue((taskList.get(3))instanceof EventTask);
+		assertTrue(temp3.getTaskName().equals("D"));
+
 		Controller.executeCMD("undo");
+		taskList=Controller.getTaskList();
+		assertTrue((taskList.get(0))instanceof FloatingTask);
+		assertTrue(taskList.get(0).getTaskName().equals("A"));
+		
 		Controller.executeCMD("search A");
 		Controller.executeCMD("display all");
 		Controller.executeCMD("delete 1-3");
@@ -53,17 +101,6 @@ public class SystematicTest {
 		Controller.executeCMD("save");
 		Controller.executeCMD("set path newfolder\\");
 		Controller.executeCMD("save");
-		
-		taskList=Controller.getTaskList();
-		assertTrue((taskList.get(0))instanceof FloatingTask);
-		assertTrue(taskList.get(0).getTaskName().equals("A"));
-		assertTrue((taskList.get(1))instanceof FloatingTask);
-		assertTrue(taskList.get(1).getTaskName().equals("ABC"));
-		assertTrue((taskList.get(2))instanceof EventTask);
-		assertTrue(taskList.get(2).getTaskName().equals("B"));
-		assertTrue((taskList.get(3))instanceof DeadlineTask);
-		assertTrue(taskList.get(3).getTaskName().equals("C"));
-		
 		assertTrue((new File("newfolder\\")).exists());
 		assertTrue((new File("newfolder\\silentjarvis.fxml")).exists());
 	}
