f9ea674bb1d110f804cc1f68522d26f3d5a04263
Pure_Sero <pure943597795@gmail.com>
2015-11-04 11:56:29 +0800

    update GUI try

--- src/view/GUI_try.java
+++ src/view/GUI_try.java
@@ -27,14 +27,14 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class GUI_try extends Application {
-	protected static String userCommand;
+	protected static String command;
 	protected static String returnCommand;
 	protected static double xOffset = 0;
 	protected static double yOffset = 0;
 	private static Logger logger = Logger.getLogger("MotionCatcher");
-	public static Image listBkImage1;
-	public static Image listBkImage2;
-	public static Image listBkImage;
+	protected Image backgroundImage;
+	protected Image iconImage;
+	protected String title="Slent Jarvis";
 
 	public static void main(String args[]) {
 		launch(args);
@@ -42,116 +42,56 @@ public class GUI_try extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Slent Jarvis");
-		primaryStage.initStyle(StageStyle.UNDECORATED);
-		primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
-		primaryStage.show();
+		loadImage();
+		
+		initialStage(primaryStage);
 
+		buildView(primaryStage);
 		
-		
+		primaryStage.show();
+	}
+
+	private void loadImage() {
+		backgroundImage=new Image(getClass().getResourceAsStream("back3.png"));
+		iconImage=new Image(getClass().getResourceAsStream("icon.png"));
+	}
+
+	private void buildView(Stage primaryStage) {
 		GridPane grid = new GridPane();
 		grid.setHgap(5);
 		grid.setVgap(20);
 		grid.setPadding(new Insets(10, 5, 5, 25));
 
-		//*set background
-		Image background = new Image(getClass().getResourceAsStream("back4.png"));
-		ImageView bk = new ImageView(background);
-		Group backg = new Group();
-		backg.getChildren().addAll(bk,grid);
+		ImageView backgroung = new ImageView(backgroundImage);
+		Group back = new Group();
+		back.getChildren().addAll(backgroung,grid);
 		
+		StackPane SystemMessageBlock= new StackPane();
+		SystemMessageBlock=buildSysMsgBlk(SystemMessageBlock);
+	    grid.add(SystemMessageBlock, 0, 1);
 		
-		/*Image iconYui = new Image(getClass().getResourceAsStream("transparency.png"));
-		grid.add(new ImageView(iconYui), 0, 0);*/
+		ScrollPane TaskDisplayBlock = new ScrollPane();
+		TaskDisplayBlock=buildTskDisBlk(TaskDisplayBlock);
+		grid.add(TaskDisplayBlock, 0, 2);
 
-		// add text box to show message
-		/*final TextArea showBox = new TextArea();
-		showBox.setPrefSize(300, 100);
-		/*Background back=new Background(new BackgroundImage(new Image(getClass().getResourceAsStream("qqq.png")), null , null, null, null));
-		showBox.setBackground(back);*/
-		//showBox.setStyle("-fx-background-color: rgba(0, 100, 100, 0.5); -fx-background-radius: 10;");
-		/*grid.add(showBox, 0, 1);
-		showBox.setEditable(false);*/
-		Font font=Font.font("Stencil Std", FontWeight.BOLD, FontPosture.REGULAR, 22);
-		
-		final Label label = new Label("Welcome to SilentJarvis!");
-	    label.setFont(font);
-	    label.setTextFill(Color.web("#ffffff"));
+		TextField userCommandBlock = new TextField();
+		userCommandBlock.requestFocus();
+		grid.add(userCommandBlock, 0, 3);
 
-	    final Label label3 = new Label("tasks are listed below");
-	    label3.setFont(font);
-	    label3.setTextFill(Color.web("#ffffff"));
-	    
-	    font=Font.font("Stencil Std", FontWeight.BOLD, FontPosture.REGULAR, 30);
-	    
-	    final Label label2 = new Label("Today's");
-	    label2.setFont(font);
-	    label2.setTextFill(Color.web("#ffffff"));
-	    
-
-	    //label2.setStyle("-fx-text-fill: goldenrod; -fx-font-style: italic; -fx-font-weight: bold; -fx-padding: 0 0 20 0;");
-
-	    StackPane glass = new StackPane();
-	    glass.setPrefSize(300, 120);
-	    grid.setVgap(20);
-	    glass.getChildren().add(label);
-	    StackPane.setAlignment(label, Pos.TOP_CENTER);
-	    glass.getChildren().add(label2);
-	    StackPane.setAlignment(label2, Pos.CENTER);
-	    glass.getChildren().add(label3);
-	    StackPane.setAlignment(label3, Pos.BOTTOM_CENTER);
-	    //glass.getChildren().remove(label);
-	    grid.add(glass, 0, 1);
-		
-		ScrollPane eventPane = new ScrollPane();
-		eventPane.setPrefSize(300, 420);
-		eventPane.setOpacity(0.9);
-		eventPane.setHbarPolicy(ScrollBarPolicy.NEVER);
-		grid.add(eventPane, 0, 2);
-
-		// add command box
-		TextField userCommandBox = new TextField();
-		grid.add(userCommandBox, 0, 3);
-		userCommandBox.requestFocus();
-	
-		// set exit button
-		/*Image imageExit = new Image(getClass().getResourceAsStream("pink.png"));
-		ImageView exit = new ImageView(imageExit);
-	    exit.setOnMouseClicked(new EventHandler<MouseEvent>(){
-	    	@Override
-	    	public void handle(MouseEvent event){
-	    		Event.fireEvent(primaryStage, new WindowEvent(primaryStage, WindowEvent.WINDOW_CLOSE_REQUEST ));
-	    	}
-	    });
-	    grid.add(exit, 0, 4);*/
-		
-		GridPane eventGrid = new GridPane();
-		eventGrid.setHgap(3);
-		eventGrid.setVgap(1);
-
-		// Control the dragging of stage
 		DragController.dragStage(grid, primaryStage);
 
-		// set the scene
-		Scene scene = new Scene(backg, 350, 660);
+		Scene scene = new Scene(back, 350, 660);
 		primaryStage.setScene(scene);
-
-		// initialize GUI and Logic
-
-		
-		/*Font font= new Font(32);
-		showBox.setFont( font );
-		showBox.appendText("hhhhh\n");*/
 		
 		// catch the motion of users
-		userCommandBox.setOnKeyPressed(new EventHandler<KeyEvent>() {
+		userCommandBlock.setOnKeyPressed(new EventHandler<KeyEvent>() {
 			@Override
 			public void handle(KeyEvent event) {
 				if (event.getCode().equals(KeyCode.ENTER)) {
-					GUI_try.userCommand = userCommandBox.getText();
+					GUI_try.userCommand = userCommandBlock.getText();
 					// link with logic
 					System.out.print(GUI_try.userCommand);
-					userCommandBox.clear();
+					userCommandBlock.clear();
 					// link with logic
 					if (!GUI_try.userCommand.equals("")) {
 
@@ -168,13 +108,54 @@ public class GUI_try extends Application {
 				}
 
 				if (event.getCode().equals(KeyCode.UP)) {
-					eventPane.setVvalue(eventPane.getVvalue() - 0.3f);
+					TaskDisplayBlock.setVvalue(TaskDisplayBlock.getVvalue() - 0.3f);
 				}
 				if (event.getCode().equals(KeyCode.DOWN)) {
-					eventPane.setVvalue(eventPane.getVvalue() + 0.3f);
+					TaskDisplayBlock.setVvalue(TaskDisplayBlock.getVvalue() + 0.3f);
 				}
 			}
 		});
-		primaryStage.show();
+	}
+
+	private ScrollPane buildTskDisBlk(ScrollPane TaskDisplayBlock) {
+		TaskDisplayBlock.setPrefSize(300, 420);
+		TaskDisplayBlock.setOpacity(0.9);
+		TaskDisplayBlock.setHbarPolicy(ScrollBarPolicy.NEVER);
+		
+		return TaskDisplayBlock;
+	}
+
+	private StackPane buildSysMsgBlk(StackPane SystemMessageBlock) {
+		Font commonFont=Font.font("Stencil Std", FontWeight.BOLD, FontPosture.REGULAR, 22);
+		Font highlightFont=Font.font("Stencil Std", FontWeight.BOLD, FontPosture.REGULAR, 30);
+		Color commonColor=Color.web("#039ed3");
+		
+		final Label line_1 = new Label("Welcome to SilentJarvis!");
+	    line_1.setFont(commonFont);
+	    line_1.setTextFill(commonColor);
+	    
+	    final Label line_2 = new Label("Today's");
+	    line_2.setFont(highlightFont);
+	    line_2.setTextFill(commonColor);
+	    
+	    final Label line_3 = new Label("tasks are listed below");
+	    line_3.setFont(commonFont);
+	    line_3.setTextFill(commonColor);
+
+	    SystemMessageBlock.setPrefSize(300, 120);
+	    SystemMessageBlock.getChildren().add(line_1);
+	    StackPane.setAlignment(line_1, Pos.TOP_CENTER);
+	    SystemMessageBlock.getChildren().add(line_2);
+	    StackPane.setAlignment(line_2, Pos.CENTER);
+	    SystemMessageBlock.getChildren().add(line_3);
+	    StackPane.setAlignment(line_3, Pos.BOTTOM_CENTER);
+	    
+		return SystemMessageBlock;
+	}
+
+	private void initialStage(Stage primaryStage) {
+		primaryStage.setTitle(title);
+		primaryStage.initStyle(StageStyle.UNDECORATED);
+		primaryStage.getIcons().add(iconImage);
 	}
 }
