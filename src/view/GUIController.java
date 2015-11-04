c8f98bd426975985681b3440237a4d77c935134a
Pure_Sero <pure943597795@gmail.com>
2015-11-04 22:52:07 +0800

    update

--- src/view/GUIController.java
+++ src/view/GUIController.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import controller.Controller;
 import model.*;
@@ -8,15 +9,18 @@ public class GUIController {
 	static ArrayList<Task> TaskList;
 
 	protected static void showRecentList() {
-		TaskList = Controller.getTodayTaskList();		
+		TaskList = Controller.getTodayTaskList();
+		showList();
+	}
+
+	private static void showList() {
+		// TODO Auto-generated method stub
+
 	}
 
 	private static String getCommandType(String command, int i) {
 		return command.trim().split("\\s+")[i];
 	}
-	
-
-
 
 	public static void execute(String command) {
 		String commandType = getCommandType(command, 0);
@@ -49,10 +53,10 @@ public class GUIController {
 			executeArchive(command);
 			break;
 		case "unarchive":
-			executeUnarchive(command);
+			executeUnComOrArc(command);
 			break;
 		case "uncomplete":
-			executeUncomplete(command);
+			executeUnComOrArc(command);
 			break;
 		case "set":
 			executeSet(command);
@@ -65,75 +69,129 @@ public class GUIController {
 		}
 	}
 
-	private static void executeUncomplete(String command) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private static void executeUnarchive(String command) {
-		// TODO Auto-generated method stub
-		
+	private static void executeUnComOrArc(String command) {
+		try {
+			Controller.executeCMD(command);
+			TaskList = Controller.getTaskList();
+			showList();
+			GUIMain.showAll();
+			GUIMain.showUnComOrArc();;
+		} catch (IOException e) {
+			GUIMain.showError();
+		}
 	}
 
 	private static void executeArchive(String command) {
-		// TODO Auto-generated method stub
-		
+		try {
+			Controller.executeCMD(command);
+			TaskList = Controller.getTaskList();
+			showList();
+			GUIMain.showAll();
+			GUIMain.showArchive();
+		} catch (IOException e) {
+			GUIMain.showError();
+		}
 	}
 
 	private static void executeComplete(String command) {
-		// TODO Auto-generated method stub
-		
+		try {
+			Controller.executeCMD(command);
+			TaskList = Controller.getTaskList();
+			showList();
+			GUIMain.showAll();
+			GUIMain.showComplete();
+		} catch (IOException e) {
+			GUIMain.showError();
+		}
 	}
 
 	private static void executeUpdate(String command) {
-		// TODO Auto-generated method stub
-		
+		try {
+			Controller.executeCMD(command);
+			TaskList = Controller.getTaskList();
+			showList();
+			GUIMain.showAll();
+			GUIMain.showUpdate();
+		} catch (IOException e) {
+			GUIMain.showError();
+		}
 	}
 
 	private static void executeAll(String command) {
-		// TODO Auto-generated method stub
-		
+		try {
+			Controller.executeCMD(command);
+			TaskList = Controller.getTaskList();
+			showList();
+			GUIMain.showAll();
+		} catch (IOException e) {
+			GUIMain.showError();
+		}
 	}
 
 	private static void executeSave(String command) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private static void executeLoad(String command) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private static void executeSearch(String command) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private static void executeDelete(String command) {
-		// TODO Auto-generated method stub
-		
+		try {
+			Controller.executeCMD(command);
+			TaskList = Controller.getTaskList();
+			showList();
+			GUIMain.showAll();
+			GUIMain.showDelete();
+		} catch (IOException e) {
+			GUIMain.showError();
+		}
 	}
 
 	private static void executeShowArchived(String command) {
-		// TODO Auto-generated method stub
-		
+		try {
+			Controller.executeCMD(command);
+			TaskList = Controller.getArchivedList();
+			showList();
+			GUIMain.showArchived();
+		} catch (IOException e) {
+			GUIMain.showError();
+		}
 	}
 
 	private static void executeSet(String command) {
-		switch (getCommandType(command, 1)) {
-		case "path":
-			GUIMain.showSetPath();
-			break;
-		case "filename":
-			GUIMain.showSetFilename();
-			break;
+		try {
+			Controller.executeCMD(command);
+			switch (getCommandType(command, 1)) {
+			case "path":
+				GUIMain.showSetPath();
+				break;
+			case "filename":
+				GUIMain.showSetFilename();
+				break;
+			}
+		} catch (IOException e) {
+			GUIMain.showError();
 		}
 	}
 
 	private static void executeAdd(String command) {
-		// TODO Auto-generated method stub
-		
+		try {
+			Controller.executeCMD(command);
+			TaskList = Controller.getTaskList();
+			showList();
+			GUIMain.showAll();
+			GUIMain.showAdd();
+		} catch (IOException e) {
+			GUIMain.showError();
+		}
 	}
 
 }
